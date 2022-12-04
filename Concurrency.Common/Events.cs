f5660a611bb1aff86c892ded179065cf5501fc96
Titouan CREACH <t.creach@unitee.io>
2022-12-04 15:05:30 +0100

    feat: scaffold

---
+++ Concurrency.Common/Events.cs
@@ -0,0 +1,14 @@
+﻿using Unitee.EventDriven.Attributes;
+
+namespace Concurrency.Common;
+
+[Subject("CONCURRENCY_START")]
+public record StartEvt();
+
+[Subject("CONCURRENCY_EVENT")]
+public record Evt(int Count);
+
+[Subject("CONCURRENCY_STOP")]
+public record StopEvt();
+
+
---
+++ Concurrency.Producer/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Diagnostics;
+using Concurrency.Common;
+using StackExchange.Redis;
+using Unitee.EventDriven.RedisStream;
+
+var eventPerSecond = 2;
+
+var running = true;
+
+Console.CancelKeyPress += delegate (object? sender, ConsoleCancelEventArgs e)
+{
+    e.Cancel = true;
+    running = false;
+};
+
+var cs = Environment.GetEnvironmentVariable("Redis__ConnectionString");
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(cs!);
+
+var publisher = new RedisStreamPublisher(redis);
+
+var count = 0;
+await publisher.PublishAsync(new StartEvt());
+
+while (running)
+{
+    var sw = Stopwatch.StartNew();
+    await publisher.PublishAsync(new Evt(count));
+    count++;
+    sw.Stop();
+    var toWait = TimeSpan.FromSeconds(1.0 / eventPerSecond) - sw.Elapsed;
+
+    Console.WriteLine(toWait);
+
+    await Task.Delay(TimeSpan.FromSeconds(1.0 / eventPerSecond) - sw.Elapsed);
+}
+
+await publisher.PublishAsync(new StopEvt());
+
+Console.WriteLine("Stopping with {0} events sent", count);
